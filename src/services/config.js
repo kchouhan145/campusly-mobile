c0b3a8c538a40ae8a48ee4c8cd50f110bfd9efc5
Kartik Chouhan <k4studies@gmail.com>
2026-06-06 23:27:35 +0530

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -2,4 +2,4 @@ import { Platform } from 'react-native';
 
 const LOCAL_API_BASE = Platform.OS === 'android' ? 'http://10.0.2.2:5000' : 'http://localhost:5000';
 
-export const API_BASE = __DEV__ ? LOCAL_API_BASE : 'https://campusly-backend-zou3.onrender.com';
+export const API_BASE = __DEV__ ? LOCAL_API_BASE : 'YOUR_API_LINK';
